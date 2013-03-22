dde4375cb1265f5a4aa46bab6c1bee43291e1e33
Devi <asldevi@gmail.com>
2013-03-22 14:43:30 +0530

    get rid of dburl

--- settings.py
+++ settings.py
@@ -5,10 +5,6 @@ from web.db import dburl2dict
 production_mode = os.environ.get('PRODUCTION_MODE', False)
 render = web.template.render('templates/', cache=production_mode)
 
-if 'DATABASE_URL' in os.environ:
-    db_url = os.environ.get('DATABASE_URL')
-else:
-    db_url = "postgres://%s:''@localhost/sridhar4mla" % os.environ.get('USER')
-db = web.database(db_url)
+db = web.database(dbn='postgres', name='sridhar4mla', pw='')
 
 
